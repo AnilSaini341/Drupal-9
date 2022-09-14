78cf037f8f0352874d72f5e24d8a40ec6ce0a4d1
Anil Saini <Anil.Saini@mashvirtual.in>
2022-09-14 17:00:10 +0530

    View Format view pager And Install view bootstrap Module

---
+++ sites/default/files/js/js_sd5WC_jJJLevYnTOhN9NLx9mrfo_YQAhfaRLVxSWp2M.js
@@ -0,0 +1,142 @@
+/**
+ * @file
+ * JavaScript behaviors for element help text (tooltip).
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  // @see https://atomiks.github.io/tippyjs/v5/all-props/
+  // @see https://atomiks.github.io/tippyjs/v6/all-props/
+  Drupal.webform = Drupal.webform || {};
+  Drupal.webform.elementHelpIcon = Drupal.webform.elementHelpIcon || {};
+  Drupal.webform.elementHelpIcon.options = Drupal.webform.elementHelpIcon.options || {};
+
+  /**
+   * Element help icon.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformElementHelpIcon = {
+    attach: function (context) {
+      if (!window.tippy) {
+        return;
+      }
+
+      // Hide on escape.
+      // @see https://atomiks.github.io/tippyjs/v6/plugins/#hideonesc
+      //
+      // Converted from ES6 to ES5.
+      // @see https://babeljs.io/repl/
+      var hideOnEsc = {
+        name: 'hideOnEsc',
+        defaultValue: true,
+        fn: function fn(_ref) {
+          var hide = _ref.hide;
+
+          function onKeyDown(event) {
+            if (event.keyCode === 27) {
+              hide();
+            }
+          }
+
+          return {
+            onShow: function onShow() {
+              document.addEventListener('keydown', onKeyDown);
+            },
+            onHide: function onHide() {
+              document.removeEventListener('keydown', onKeyDown);
+            }
+          };
+        }
+      };
+
+      $(context).find('.js-webform-element-help').once('webform-element-help').each(function () {
+        var $link = $(this);
+
+        $link.on('click', function (event) {
+          // Prevent click from toggling <label>s wrapped around help.
+          event.preventDefault();
+        });
+
+        var options = $.extend({
+          content: $link.attr('data-webform-help'),
+          delay: 100,
+          allowHTML: true,
+          interactive: true,
+          plugins: [hideOnEsc]
+        }, Drupal.webform.elementHelpIcon.options);
+
+        tippy(this, options);
+      });
+    }
+  };
+
+})(jQuery, Drupal);
+;
+/**
+ * @file
+ * JavaScript behaviors for select menu.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Disable select menu options using JavaScript.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformSelectOptionsDisabled = {
+    attach: function (context) {
+      $('select[data-webform-select-options-disabled]', context).once('webform-select-options-disabled').each(function () {
+        var $select = $(this);
+        var disabled = $select.attr('data-webform-select-options-disabled').split(/\s*,\s*/);
+        $select.find('option').filter(function isDisabled() {
+          return ($.inArray(this.value, disabled) !== -1);
+        }).attr('disabled', 'disabled');
+      });
+    }
+  };
+
+})(jQuery, Drupal);
+;
+/**
+ * @file
+ * JavaScript behaviors for help.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Handles disabling help dialog for mobile devices.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches the behavior for disabling help dialog for mobile devices.
+   */
+  Drupal.behaviors.webformHelpDialog = {
+    attach: function (context) {
+      $(context).find('.button-webform-play').once('webform-help-dialog').on('click', function (event) {
+        if ($(window).width() < 768) {
+          event.stopImmediatePropagation();
+        }
+      }).each(function () {
+        // Must make sure that this click event handler is execute first and
+        // before the Ajax dialog handler.
+        // @see http://stackoverflow.com/questions/2360655/jquery-event-handlers-always-execute-in-order-they-were-bound-any-way-around-t
+        var handlers = $._data(this, 'events')['click'];
+        var handler = handlers.pop();
+        // Move it at the beginning.
+        handlers.splice(0, 0, handler);
+      });
+    }
+  };
+
+})(jQuery, Drupal);
+;
